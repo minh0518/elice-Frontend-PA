bb8d1e9b4c04ddd0c468b2d78b98764a39691f57
minh0518 <rubics@naver.com>
2024-03-25 22:21:03 +0900

    feat: CourseCard 마크업

---
+++ src/app/_components/_filter/FilterResults.tsx
@@ -0,0 +1,19 @@
+import CourseCard from '../_ui/CourseCard';
+import styles from './FilterResults.module.scss';
+
+const FilterResults = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.total_count}>전체 12개</div>
+      <div className={styles.course_cards}>
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+      </div>
+    </section>
+  );
+};
+
+export default FilterResults;
