f38da6706a39445bf60f7f659f4ec4d5bda12bf2
minh0518 <rubics@naver.com>
2024-03-25 22:10:15 +0900

    feat: 페이지 전체 레이아웃 및 검색창 마크업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import FilterResults from './_components/_filter/FilterResults';
+import FilterTable from './_components/_filter/FilterTable';
+import SearchArea from './_components/_search/SearchArea';
+import styles from './page.module.scss';
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className={styles.container}>
+      <section className={styles.content}>
+        <SearchArea />
+        <FilterTable />
+        <FilterResults />
+      </section>
+    </main>
+  );
 }
