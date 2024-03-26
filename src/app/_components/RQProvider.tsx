4d76532e2fba0b6d562f9169c984e8c6b6f9317c
minh0518 <rubics@naver.com>
2024-03-27 01:37:13 +0900

    feat: Tanstack-Query 세팅 및 서버사이드 패칭 적용

---
+++ src/app/_components/RQProvider.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import React, { ReactNode, useState } from 'react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+function RQProvider({ children }: { children: ReactNode }) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          retryOnMount: true,
+          refetchOnReconnect: false,
+          retry: false,
+        },
+      },
+    }),
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={process.env.NODE_ENV === 'development'} />
+    </QueryClientProvider>
+  );
+}
+
+export default RQProvider;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import Results from './_components/_result/Results';
 import FilterTable from './_components/_filter/FilterTable';
 import SearchArea from './_components/_search/SearchArea';
 import styles from './page.module.scss';
-import { getFilterCondition } from './utils/filter';
+import { getFilterCondition } from './_utils/filter';
+import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
+import { generateQueryKey } from './_utils/generateQueryKey';
+import RQProvider from './_components/RQProvider';
 
 export type FilterProps = {
   searchParams: {
@@ -18,18 +21,35 @@ export default async function Home({ searchParams }: FilterProps) {
   const filterConditions = getFilterCondition({ searchParams });
   const encodeUrl = encodeURIComponent(JSON.stringify(filterConditions));
 
-  const res = await fetch(`http://localhost:3000/api/get/list?encodeUrl=${encodeUrl}`, {
-    method: 'GET',
-    cache: 'no-store',
+  const mappedKey = JSON.stringify(generateQueryKey({ searchParams }));
+  const queryClient = new QueryClient();
+  await queryClient.fetchQuery({
+    queryKey: ['selected', mappedKey],
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3000/api/get/list?encodeUrl=${encodeUrl}`, {
+        method: 'GET',
+        cache: 'no-store',
+      });
+      return await response.json();
+    },
   });
+  const dehydratedState = dehydrate(queryClient);
+  // const res = await fetch(`http://localhost:3000/api/get/list?encodeUrl=${encodeUrl}`, {
+  //   method: 'GET',
+  //   cache: 'no-store',
+  // });
 
   return (
-    <main className={styles.container}>
-      <section className={styles.content}>
-        <SearchArea />
-        <FilterTable />
-        <Results />
-      </section>
-    </main>
+    <RQProvider>
+      <main className={styles.container}>
+        <section className={styles.content}>
+          <SearchArea />
+          <FilterTable />
+          <HydrationBoundary state={dehydratedState}>
+            <Results mappedKey={mappedKey} encodeUrl={encodeUrl} />
+          </HydrationBoundary>
+        </section>
+      </main>
+    </RQProvider>
   );
 }
