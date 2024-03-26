65a8a6a1bc2c0c67e3ad365f28a566531ae9d9b4
minh0518 <rubics@naver.com>
2024-03-27 03:53:07 +0900

    fix: 기존 필터링 로직에 검색창에 입력된 문자열 추가

--- src/app/_utils/filter.ts
+++ src/app/_utils/filter.ts
@@ -9,16 +9,18 @@ type FilterType =
   | { status: number }
   | [];
 
+type SearchKeyword = { title: string };
 type FilterCondition = {
   $or: FilterType[];
 };
 
 type BaseFilter = {
-  $and: ({ title: string } | FilterCondition | { is_datetime_enrollable: boolean })[];
+  $and: (SearchKeyword | FilterCondition | { is_datetime_enrollable: boolean })[];
 };
 
 export const getFilterCondition = ({ searchParams }: FilterProps) => {
   const INDEX_INFO = {
+    keyword: 0,
     courseType: 7,
     format: 6,
     category: 1,
@@ -42,40 +44,53 @@ export const getFilterCondition = ({ searchParams }: FilterProps) => {
   };
 
   for (let key in searchParams) {
-    // 쿼리스트링에 선택된 숫자
-    const searchParamsNumbers = searchParams[key as keyof FilterProps['searchParams']];
+    // 쿼리스트링 전체 정보 객체
+    const searchParamsInfo = searchParams[key as keyof FilterProps['searchParams']];
 
-    if (searchParamsNumbers) {
-      let numbers;
-      if (Array.isArray(searchParamsNumbers)) numbers = searchParamsNumbers.map(Number);
-      else numbers = [Number(searchParamsNumbers)];
+    if (searchParamsInfo) {
+      // 쿼리스트링 객체 내부 값
+      let values;
+      if (Array.isArray(searchParamsInfo)) values = searchParamsInfo.map(Number);
+      if (!Array.isArray(searchParamsInfo)) {
+        // searchParamsValues가 단일 값이라면 숫자(카테고리)인지 문자열(title)인지 판별
+        const numberValue = Number(searchParamsInfo);
+        values = isNaN(numberValue) ? searchParamsInfo : [numberValue];
+      }
 
-      for (let number of numbers) {
-        // BASE_FILTER에서 변경할 인덱스
-        const updateTargetIndex = INDEX_INFO[key as keyof FilterProps['searchParams']];
+      // 카테고리 필터링 처리
+      if (Array.isArray(values)) {
+        for (let value of values) {
+          // BASE_FILTER에서 변경할 인덱스
+          const updateTargetIndex = INDEX_INFO[key as keyof FilterProps['searchParams']];
 
-        // BASE_FILTER에서 변경할 인덱스의 배열
-        const filterArr = (BASE_FILTER.$and[updateTargetIndex] as FilterCondition).$or;
+          // BASE_FILTER에서 변경할 인덱스의 배열
+          const filterArr = (BASE_FILTER.$and[updateTargetIndex] as FilterCondition).$or;
 
-        // FILTER_CATEGORY에서 현재 쿼리스트링에 맞는 정보 추출 (깊은복사)
-        const targetCategory = JSON.parse(
-          JSON.stringify(FILTER_CATEGORY.find((item) => item.query === key)),
-        );
+          // FILTER_CATEGORY에서 현재 쿼리스트링에 맞는 필터링 정보 추출 (깊은복사)
+          const targetCategory = JSON.parse(
+            JSON.stringify(FILTER_CATEGORY.find((item) => item.query === key)),
+          );
 
-        if (
-          targetCategory &&
-          targetCategory.filterInfo[number - targetCategory.startIndex] !== undefined
-        ) {
-          filterArr?.push(targetCategory.filterInfo[number - targetCategory.startIndex]);
+          if (
+            targetCategory &&
+            targetCategory.filterInfo[value - targetCategory.startIndex] !== undefined
+          ) {
+            filterArr?.push(targetCategory.filterInfo[value - targetCategory.startIndex]);
+          }
         }
+      } else {
+        // title 필터링 처리
+        BASE_FILTER.$and[0] = { title: `%${String(values)}%` };
       }
     }
   }
 
-  const shallow = BASE_FILTER.$and.filter((i, index) => {
-    if (index === 0 || index === 2 || index === 3 || index === 8) return true;
-    else {
-      return (i as FilterCondition).$or.length;
+  const shallow = BASE_FILTER.$and.filter((item, index) => {
+    if (index === 2 || index === 3 || index === 8) return true;
+    if (index === 0) {
+      return item as SearchKeyword;
+    } else {
+      return (item as FilterCondition).$or.length;
     }
   });
   BASE_FILTER.$and = shallow;
