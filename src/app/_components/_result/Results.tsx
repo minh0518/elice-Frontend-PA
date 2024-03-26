919bdf71e930cd8e3a400503ccdce59fead9cfe8
minh0518 <rubics@naver.com>
2024-03-27 01:40:50 +0900

    feat: 패칭된 데이터로 CourseCard UI적용

--- src/app/_components/_result/Results.tsx
+++ src/app/_components/_result/Results.tsx
@@ -1,19 +1,41 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
 import CourseCard from '../_ui/CourseCard';
 import styles from './Results.module.scss';
+import { OrgCourseListResponse } from '@/types/const';
+import { Fragment } from 'react';
+import { useFetchData } from '@/app/_hooks/useFetchData';
 
-const Results = () => {
+interface Props {
+  mappedKey: string;
+  encodeUrl: string;
+}
+
+const Results = ({ mappedKey, encodeUrl }: Props) => {
+  const data = useFetchData(mappedKey, encodeUrl);
   return (
     <section className={styles.container}>
-      <div className={styles.total_count}>전체 12개</div>
-      <div className={styles.course_cards}>
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
-      </div>
+      {data && (
+        <>
+          <div className={styles.total_count}>전체 {data.courseCount}개</div>
+          <div className={styles.course_cards}>
+            {data.courses.map((item, index) => {
+              return (
+                <Fragment key={index}>
+                  <CourseCard
+                    title={item.title}
+                    description={item.shortDescriptioin}
+                    logoFileUrl={item.logoFileUrl || ''}
+                    enrollType={item.enrollType}
+                    isFree={item.isFree}
+                  />
+                </Fragment>
+              );
+            })}
+          </div>
+        </>
+      )}
     </section>
   );
 };
