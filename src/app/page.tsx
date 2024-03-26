32719cfa036cef11a8e36ffc958e83349906a803
minh0518 <rubics@naver.com>
2024-03-26 23:08:08 +0900

    feat: 서버 컴포넌트 router handler api 호출

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,27 @@ import Results from './_components/_result/Results';
 import FilterTable from './_components/_filter/FilterTable';
 import SearchArea from './_components/_search/SearchArea';
 import styles from './page.module.scss';
-export default function Home() {
+import { getFilterCondition } from './utils/filter';
+
+export type FilterProps = {
+  searchParams: {
+    courseType?: string;
+    format?: string;
+    category?: string;
+    level?: string;
+    programmingLanguage?: string;
+    price?: string;
+  };
+};
+export default async function Home({ searchParams }: FilterProps) {
+  const filterConditions = getFilterCondition({ searchParams });
+  const encodeUrl = encodeURIComponent(JSON.stringify(filterConditions));
+
+  const res = await fetch(`http://localhost:3000/api/get/list?encodeUrl=${encodeUrl}`, {
+    method: 'GET',
+    cache: 'no-store',
+  });
+
   return (
     <main className={styles.container}>
       <section className={styles.content}>
