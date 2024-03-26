eac5a14a24aa812fc73e76afc0f368bb4b89b1b3
조민호 <78631876+minh0518@users.noreply.github.com>
2024-03-26 23:03:54 +0900

    [#11] 필터링 URL과 파라미터 (filter_conditions) 구현 (#12)
    * chore: 조건부 스타일을 위한 classnames설치
    
    * feat: 선택된 Chip에 따른 URL쿼리 추가 및 조건부 스타일 적용
    
    * feat: 카테고리별 고유 필터 정보 추가
    
    * feat: 선택한 Chip에 따른 필터링 파라미터(filter_conditions) 구현
    
    * fix: 쿼리스트링 삭제 후 페이지 변경 및 누락된 필터링 조건 추가

--- src/app/_components/_ui/Chip.tsx
+++ src/app/_components/_ui/Chip.tsx
@@ -2,14 +2,58 @@
 
 import { filter_category } from '@/types/const';
 import styles from './Chip.module.scss';
+import { useRouter, useSearchParams } from 'next/navigation';
+import classNames from 'classnames';
 
 interface Props {
   query: filter_category[number]['query'];
   values: filter_category[number]['values'][number];
+  index: number;
+  startIndex: number;
+  filterInfo: filter_category[number]['filterInfo'];
 }
 
-const Chip = ({ query, values }: Props) => {
-  return <button className={styles.container}>{values}</button>;
+const Chip = ({ query, values, index, startIndex }: Props) => {
+  const currentChipNumber = startIndex + index;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const eachQuery = searchParams.toString().split('&');
+
+  let existQuery: { flag: boolean; index: number } = { flag: false, index: 0 };
+  eachQuery.forEach((info, index) => {
+    const [currentQuery, value] = info.split('=');
+    if (currentQuery === query && Number(value) === currentChipNumber) {
+      existQuery.flag = true;
+      existQuery.index = index;
+    }
+  });
+
+  const onChipClick = () => {
+    if (existQuery.flag) {
+      const shallow = [...eachQuery];
+      shallow.splice(existQuery.index, 1);
+      router.push(`?${shallow.join('&')}`);
+    }
+
+    if (!existQuery.flag) {
+      if (!searchParams.toString()) {
+        const url = `?${query}=${startIndex + index}`;
+        router.push(url);
+      } else {
+        const url = `?${searchParams.toString()}&${query}=${startIndex + index}`;
+        router.push(url);
+      }
+    }
+  };
+
+  return (
+    <button
+      className={classNames([styles.container, existQuery.flag && styles.clicked])}
+      onClick={() => onChipClick()}
+    >
+      {values}
+    </button>
+  );
 };
 
 export default Chip;
---
+++ src/app/utils/filter.ts
@@ -0,0 +1,84 @@
+import { FILTER_CATEGORY } from '@/config/const';
+import { FilterProps } from '../page';
+
+type FilterType =
+  | { tag_id: number }
+  | { course_type: number }
+  | { $or: { course_type: number }[] }
+  | { enroll_type: number; is_free?: boolean }
+  | { status: number }
+  | [];
+
+type FilterCondition = {
+  $or: FilterType[];
+};
+
+type BaseFilter = {
+  $and: ({ title: string } | FilterCondition | { is_datetime_enrollable: boolean })[];
+};
+
+export const getFilterCondition = ({ searchParams }: FilterProps) => {
+  const INDEX_INFO = {
+    courseType: 7,
+    format: 6,
+    category: 1,
+    level: 5,
+    programmingLanguage: 4,
+    price: 3,
+  };
+
+  const BASE_FILTER: BaseFilter = {
+    $and: [
+      { title: '%%' } /** 검색 - 유지  */,
+      { $or: [] } /** category: [1] */,
+      { $or: [{ status: 2 }, { status: 3 }, { status: 4 }] } /** 유지  */,
+      { $or: [] } /** price: [3] - 유지 */,
+      { $or: [] } /** programmingLanguage: [4] */,
+      { $or: [] } /** level: [5] */,
+      { $or: [] } /** format: [6] */,
+      { $or: [] } /** courseType: [7] */,
+      { is_datetime_enrollable: true } /**  유지  */,
+    ],
+  };
+
+  for (let key in searchParams) {
+    // 쿼리스트링에 선택된 숫자
+    const searchParamsNumbers = searchParams[key as keyof FilterProps['searchParams']];
+
+    if (searchParamsNumbers) {
+      let numbers;
+      if (Array.isArray(searchParamsNumbers)) numbers = searchParamsNumbers.map(Number);
+      else numbers = [Number(searchParamsNumbers)];
+
+      for (let number of numbers) {
+        // BASE_FILTER에서 변경할 인덱스
+        const updateTargetIndex = INDEX_INFO[key as keyof FilterProps['searchParams']];
+
+        // BASE_FILTER에서 변경할 인덱스의 배열
+        const filterArr = (BASE_FILTER.$and[updateTargetIndex] as FilterCondition).$or;
+
+        // FILTER_CATEGORY에서 현재 쿼리스트링에 맞는 정보 추출 (깊은복사)
+        const targetCategory = JSON.parse(
+          JSON.stringify(FILTER_CATEGORY.find((item) => item.query === key)),
+        );
+
+        if (
+          targetCategory &&
+          targetCategory.filterInfo[number - targetCategory.startIndex] !== undefined
+        ) {
+          filterArr?.push(targetCategory.filterInfo[number - targetCategory.startIndex]);
+        }
+      }
+    }
+  }
+
+  const shallow = BASE_FILTER.$and.filter((i, index) => {
+    if (index === 0 || index === 2 || index === 3 || index === 8) return true;
+    else {
+      return (i as FilterCondition).$or.length;
+    }
+  });
+  BASE_FILTER.$and = shallow;
+
+  return BASE_FILTER;
+};
