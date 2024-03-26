40ae97f7bb4c74f3e387b2b5b32a1a847bb405c9
minh0518 <rubics@naver.com>
2024-03-26 23:33:25 +0900

    feat: 요구사항 명세의 response타입 및 데이터 추출

--- src/app/api/get/list/route.ts
+++ src/app/api/get/list/route.ts
@@ -1,3 +1,4 @@
+import { OrgCourseListResponse } from '@/types/const';
 import { NextRequest, NextResponse } from 'next/server';
 
 const ELICE_SERVICE_BASE_URL = 'https://api-rest.elice.io/org/academy/course/list';
@@ -13,6 +14,35 @@ export async function GET(request: NextRequest) {
         cache: 'no-store',
       },
     );
+
+    const data = await fetchResponse.json();
+
+    const transformedData: OrgCourseListResponse = {
+      courseCount: data.course_count,
+      courses: data.courses.map((course: any) => ({
+        courseType: course.course_type,
+        tags: course.tags,
+        title: course.title,
+        shortDescriptioin: course.short_description,
+        classType: course.class_type,
+        logoFileUrl: course.logo_file_url,
+        enrolledRolePeriod: course.enrolled_role_period,
+        enrolledRoleBeginDatetime: course.enroll_begin_datetime,
+        enrolledRoleEndDatetime: course.enroll_end_datetime,
+        beginDatetime: course.begin_datetime,
+        endDatetime: course.end_datetime,
+        isDiscounted: course.is_discounted,
+        discountedPrice: course.discounted_price,
+        discountedPriceUsd: course.discounted_price_usd,
+        discountRate: course.discount_rate,
+        price: course.price,
+        priceUsd: course.price_usd,
+        enrollType: course.enroll_type,
+        isFree: course.is_free,
+      })),
+    };
+
+    return new NextResponse(JSON.stringify(transformedData), { status: 200 });
   }
 
   return new NextResponse('데이터를 불러오는 도중 에러가 발생했습니다', { status: 500 });
